275fac845f7df5921ee1a71dd2422a71845c9fc5
Jaime Jiménez <jimenez.ing.sis@gmail.com>
2017-10-13 21:05:49 -0500

    Revert "Login form and template"
    This reverts commit 41fddb218be3cd9c3113b8c44afb8693f515a771.

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.views.generic.base import TemplateView
-
-urlpatterns = [
-    url(r'^$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-]
